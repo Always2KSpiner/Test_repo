8530fe5d34164e521b50375de9d424ad463a3db8
Always2KSpiner <chaudharyanirudh98@gmail.com>
2022-09-19 22:18:49 +0530

    updated

--- iac/terraform/test.tf
+++ iac/terraform/test.tf
@@ -8,4 +8,4 @@ If you did not attempt to sign in to your account, your password may be compromi
 
 If you'd like to automatically verify devices in the future, consider enabling two-factor authentication on your account. Visit https://docs.github.com/articles/configuring-two-factor-authentication to learn about two-factor authentication.
 
-If you decide to enable two-factor authentication, ensure you retain access to one or more account recovery methods. See https://docs.github.com/articles/configuring-two-factor-authentication-recovery-methods in the GitHub Help.
+If you decide to eniable two-factor authenticationscdcsdcscscscscdxcxzcxcsc, ensure you retain access to one or more account recovery methods. See https://docs.github.com/articles/configuring-two-factor-authentication-recovery-methods in the GitHub Help.
